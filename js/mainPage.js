1f8953f675e0bb1765a7efb4b0cb798705966e2d
wanghong-yang <549398718@qq.com>
2016-09-19 00:17:03 -0700

    Vue.js Search Highlight
    Vue.js Search Highlight

---
+++ js/mainPage.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Hubert(Hongyang) Wang on 2016/9/19.
+ */
+
+// register the grid component
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
+// highlight filter
+Vue.filter('highlight', function(words, query){
+    var iQuery = new RegExp(query, "ig");
+    return words.toString().replace(iQuery, function(matchedTxt,a,b){
+        return ('<span class=\'highlight\'>' + matchedTxt + '</span>');
+    });
+});
+
+// bootstrap the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  }
+})
